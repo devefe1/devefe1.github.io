8ac721e60d51a6f385d75b6004649124b7e87f91
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-05-29 10:02:46 -0500

    getter

---
+++ movie_app/src/main/java/com/example/movie_app/model/Cart.java
@@ -0,0 +1,55 @@
+package com.example.movie_app.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cart")
+public class Cart {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "product_price")
+    private long productPrice;
+
+    public Cart () {
+        super();
+    }
+
+    public Cart(String productName, long productPrice) {
+        this.productName = productName;
+        this.productPrice= productPrice;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public long getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(long productPrice) {
+        this.productPrice = productPrice;
+    }
+}
+
+
+
---
+++ movie_app/src/main/java/com/example/movie_app/model/User.java
@@ -0,0 +1,64 @@
+package com.example.movie_app.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+        super();
+    }
+
+    public User(String email, String userName, String password) {
+        super();
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
