5078f1d0656f3f876a7ac1216a109f95c2d760b3
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-01 13:43:36 -0500

    commit

--- cinema_app/src/App.js
+++ cinema_app/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    user: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/users');
+    const body = await response.json();
+    this.setState({ user: body, isLoading: false });
+  }
+
+  render() {
+    const {user, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>User List</h2>
+            {user.map(user =>
+              <div key={user.id}>
+                {user.firstName}
+                <br></br>
+                {user.lastName}
+                <br></br>
+                {user.email}
+              </div>
+            )}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
