c9ce5ff16523cbf0f019d0e37b2daa87aa3a2831
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-02 06:59:10 -0500

    continue

--- cinema_app/src/App.js
+++ cinema_app/src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import './App.css';
 import Search from './components/Search';
+import axios from axios;
+// import Movie from './components/movieproducts';
 
 function App() {
+    const [state, setState] = useState({
+      s: "",
+      results: [],
+      selected: {}
+    });
+
+    const apiurl = "https://api.themoviedb.org/?api_key=5ca0c1882e4a693b0bbf1ff8ccdd25a2"
+
+   
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
+    }
+  }
+   
+    const handleInput = (e) => {
+       let s = e.target.value;
+          
+        setState(prevState => {
+          return {...prevState, s: s}
+        });
+    }
+
+
   return (
     <div className="App">
+       <main>
+            <Search handleInput={handleInput} search={search} />
+            {/* <Movie /> */}
+        </main>
+
     <Router>
       <Switch>
         <Route path='/' exact component={Home} />
@@ -16,9 +53,7 @@ function App() {
         <Route path='/login' component={Login} />
       </Switch>
     </Router>
-        <main>
-            <Search />
-        </main>
+       
     </div>
   );
 }
--- cinema_app/src/components/Nav.jsx
+++ cinema_app/src/components/Nav.jsx
@@ -4,10 +4,7 @@ import { Link } from 'react-router-dom';
  const Nav = () => {
      return ( 
          <div>
-             <select name="" id="">
-                 <option value="">Movies</option>
                  <Link id='footer-links' to='/cart' > <li> Cart </li> </Link> 
-             </select>
          </div>
       );
  }
