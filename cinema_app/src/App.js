d15594137012b21133dc9782c0ea063e55257560
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-01 21:37:22 -0500

    signup

--- cinema_app/src/App.js
+++ cinema_app/src/App.js
@@ -1,44 +1,12 @@
 import React,{Component} from 'react';
+import {BrowserROuter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
+import Signup from './components/Signup'
 
-class App extends Component {
-  state = {
-    isLoading: true,
-    user: []
-  };
 
-  async componentDidMount() {
-    const response = await fetch('/users');
-    const body = await response.json();
-    this.setState({ user: body, isLoading: false });
-  }
 
-  render() {
-    const {user, isLoading} = this.state;
 
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App-intro">
-            <h2>User List</h2>
-            {user.map(user =>
-              <div key={user.id}>
-                {user.userName}
-                <br></br>
-                {user.email}
-                <br></br>
-                {user.password}
-              </div>
-            )}
-          </div>
-        </header>
-      </div>
-    );
-  }
-}
 
 export default App;
---
+++ cinema_app/src/components/Signup.jsx
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+
+
+
+class Signup extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                email : '',
+                userName: '',
+                password: ''
+            }
+        };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+handleChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    let user = {...this.state.user};
+    user[name] = value;
+    this.setState({user});
+}
+
+handleSubmit = async (e) => {
+    e.preventDefault();
+    const {user} = this.state;
+    try {
+        const response = await axios.post ('/users', user)
+        console.log(response.data)
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+render () {
+    const {user} = this.state;
+    return (
+        <div className='page'>
+            <div className= 'signupPage'>
+                <form className= 'signup' onSubmit = {this.handleSubmit} >
+                    <input type="email" placeholder= "Email" name = "email" value = {user.email || ''} onChange={this.handleChange} />
+                    <input type="text" placeholder= "UserName" name = "userName" value = {user.userName || ''} onChange={this.handleChange} />
+                    <input type="password" placeholder= "Password" name = "password" value = {user.password || ''} onChange={this.handleChange} />
+                    <button type="submit" > Sign Up! </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+}
