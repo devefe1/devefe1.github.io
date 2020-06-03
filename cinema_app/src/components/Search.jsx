a9c487276c2931c6acf07af575bebd3b31b15fc0
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-03 00:29:22 -0500

    edit

--- cinema_app/src/components/Search.jsx
+++ cinema_app/src/components/Search.jsx
@@ -4,7 +4,7 @@ function search({handleInput, search}) {
     return (
         <section className = "searchbox-wrap">
             <input type ="text"
-             placeholder= "Search for a movie...                                                                                                               ... press Enter' when done" 
+             placeholder= "Search for a movie...                                                                                                                       ... press Enter' when done" 
              className = "searchBox"
             onChange ={handleInput}
             onKeyPress={search}
