8d307540362d4289ced0f7814a23483e4a7f667a
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-03 00:29:01 -0500

    edit

--- movie_app/src/main/java/com/example/movie_app/controller/ForumController.java
+++ movie_app/src/main/java/com/example/movie_app/controller/ForumController.java
@@ -3,8 +3,10 @@ package com.example.movie_app.controller;
 
 import com.example.movie_app.exception.ResourceNotFoundException;
 import com.example.movie_app.model.Forum;
+import com.example.movie_app.model.User;
 import com.example.movie_app.repository.ForumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +32,22 @@ public class ForumController {
     public Forum createForum(@Valid @RequestBody Forum forum) {
         return forumRepository.save(forum);
     }
+
+    @PutMapping("/forum/{id}")
+    public ResponseEntity<Forum> updateForum(@PathVariable(value = "id") Long post_Id, @Valid @RequestBody Forum forumDetails)
+            throws ResourceNotFoundException {
+        Forum forum = forumRepository.findById(post_Id)
+                .orElseThrow(() -> new ResourceNotFoundException("post not found " + post_Id));
+
+        forum.setPost(forumDetails.getPost());
+        forum.setUserName(forumDetails.getUserName());
+
+
+        final Forum updatedForum = forumRepository.save(forum);
+
+        return ResponseEntity.ok(updatedForum);
+    }
+
 
     // delete forum posts
     @DeleteMapping("/forum/{id}")
