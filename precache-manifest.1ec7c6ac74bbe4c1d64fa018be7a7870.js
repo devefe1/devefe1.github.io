fb740459ebc717f4051d3bd85d463df22587145e
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-09 19:18:28 -0500

    Updates

---
+++ precache-manifest.1ec7c6ac74bbe4c1d64fa018be7a7870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6f659043910257698003c3993712de",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "73e1f4ce649f4d95856a",
+    "url": "/movieapp/static/css/main.5f4e12d2.chunk.css"
+  },
+  {
+    "revision": "1c2886bb1d6afdde6a0b",
+    "url": "/movieapp/static/js/2.8dbb24bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieapp/static/js/2.8dbb24bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e1f4ce649f4d95856a",
+    "url": "/movieapp/static/js/main.396cc01b.chunk.js"
+  },
+  {
+    "revision": "da01881a4fd223058aa9",
+    "url": "/movieapp/static/js/runtime-main.a027ba91.js"
+  },
+  {
+    "revision": "b9d8683c40bc417764fe8af9987297ba",
+    "url": "/movieapp/static/media/picture.b9d8683c.jpg"
+  }
+]);
